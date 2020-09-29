34c1dba0acc46cbe896931fc42370e62aa6b73b5
Ethan Duffin <ethan.duffin@icloud.com>
2020-09-29 14:19:07 +0000

    I added the color to Zoog's head. I also added color to all the other parts of zoog and fixed the legs.

--- index.java
+++ index.java
@@ -7,28 +7,25 @@ void draw() {
   ellipseMode(CENTER);
   ellipse(mouseX, mouseY, 1, 1);
   println(mouseX, mouseY);
-
+  fill(mouseY)
   background(200);
   // Set CENTER mode
   ellipseMode(CENTER);
   rectMode(CENTER);
   // Draw Zoog's body
   stroke(0);
-  fill(150);
+  fill(mouseX, mouseY, 120);
   rect(mouseX, mouseY,20,100);
   // Draw Zoog's head
   stroke(0);
-  fill(255);
+  fill(mouseX, mouseY, 150);
   ellipse(mouseX, mouseY - 30,60,60);
-  // Draw Zoog's eyes
-  fill(0);
-
-  // remove the forward slashes in front of each line of code below one at a time. Remove the underline and replace with the proper code to allow your mouse to move Zoog around  💻
-
-  //ellipse(__________, __________,16,32);
-  //ellipse(__________, __________,16,32);
+  // Draw Zoog's Eyes
+  fill(mouseX, mouseY, 255);
+  ellipse(mouseX - 10, mouseY - 30,16,32);
+  ellipse(mouseX - -10, mouseY - 30,16,32);
   // Draw Zoog's legs
-  stroke(0);
-  //line(_________, __________, __________, __________);
-  //line(__________, __________, __________, __________);
+  stroke(mouseX, mouseY, 150);
+  line(mouseX - 10, mouseY + 50, mouseX - 100, mouseY + 100);
+  line(mouseX + 10, mouseY + 50, mouseX + 100, mouseY + 100);
 }
